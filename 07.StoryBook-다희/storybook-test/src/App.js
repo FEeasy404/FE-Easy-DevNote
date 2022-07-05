a077a84636e870274db4bc5d1ebf768fe3baf947
yeodahui <yeodahui.dev@gmail.com>
2022-07-05 20:25:51 +0900

    Feat: Storybook 예제 추가

---
+++ 07.StoryBook-다희/storybook-test/src/App.js
@@ -0,0 +1,11 @@
+import Button from "./components/Button.jsx";
+
+function App() {
+  return (
+    <>
+      <Button size="large" primary={true} active={false} />
+    </>
+  );
+}
+
+export default App;
